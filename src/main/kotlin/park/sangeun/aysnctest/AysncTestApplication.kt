06f55261deb8dc8cd07df28d335920ee6499bc5c
Amber <separk@yanadoocorp.com>
2025-08-06 16:58:58 +0900

    [PRACTICE-4] 프로젝트 초기 설정

---
+++ src/main/kotlin/park/sangeun/aysnctest/AysncTestApplication.kt
@@ -0,0 +1,11 @@
+package park.sangeun.aysnctest
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class AysncTestApplication
+
+fun main(args: Array<String>) {
+    runApplication<AysncTestApplication>(*args)
+}
